8d7d06eff971aceff32edad83c39addd1196ee23
Asan <busraasan1234@gmail.com>
2019-12-29 23:22:23 +0300

    debug mode closed.

--- server.py
+++ server.py
@@ -110,4 +110,4 @@ def create_app():
 
 
 application = create_app()
-application.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
+application.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
